def6aa8966ba75a6fb667417430e93933f4f20b7
Isac Moura <contatoisacmoura@outlook.com>
2022-11-06 17:58:03 -0300

    Initial Commit

---
+++ SalesTax/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SalesTax
+//
+//  Created by Isac Moura Gomes on 06/11/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
